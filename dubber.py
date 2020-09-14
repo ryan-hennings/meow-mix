b5fba7aa05170e834f38263379784adec14b8d47
ryan-hennings <ryanhennings@gmail.com>
2020-09-13 18:05:02 -0700

    pitchy

--- dubber.py
+++ dubber.py
@@ -1,7 +1,23 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
-sound = AudioSegment.from_file("cut.wav", format="wav")
-#cut = sound[:2*1000]
+def pitch(sound, octaves):
+    sample_rate = int(sound.frame_rate * (2.0 ** octaves))
+    sound = sound._spawn(sound.raw_data, overrides={'frame_rate': sample_rate})
+    return sound.set_frame_rate(44100)
+
+cut = AudioSegment.from_file("sounds/perc-g.wav", format="wav")
+
+cut = cut[:200]
+
+res = cut
+for ix in range(10):
+    res = res.append(pitch(cut, ix+1), crossfade=0)
+for ix in range(8):
+    res = res.append(pitch(cut, ix / 4.0), crossfade=1)
+for ix in range(16):
+    res = res.append(pitch(cut, (16 - ix) / 2.0), crossfade=1)
+
+play(res)
+
 #cut.export("cut.wav", format="wav")
-play(sound)
